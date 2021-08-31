bb5d1b1cf3af8aa67b9ccffae74515ba625d5a97
hjinn <hojun@hojun.local>
2021-09-01 00:04:27 +0900

    주석 제거 / 폴더 명 변경

--- agaein_web/src/pages/Home/Home.tsx
+++ agaein_web/src/pages/Home/Home.tsx
@@ -6,12 +6,6 @@ const Home = () => {
     return (
         <div style={{ backgroundColor: theme.light.background }}>
             <h1>홈 페이지</h1>
-            <img
-                width={100}
-                height={100}
-                src="/assets/image/penguin.png"
-                alt="Grapefruit slice atop a pile of other slices"
-            />
         </div>
     );
 };
