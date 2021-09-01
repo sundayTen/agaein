a104c837b690b94e937652fc10bf1a2a26219203
hojunin <dlsghwns@naver.com>
2021-09-01 14:02:11 +0900

    인풋 너비 설정

--- agaein_web/src/components/Input/index.tsx
+++ agaein_web/src/components/Input/index.tsx
@@ -11,7 +11,15 @@ const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     // }));
 
     // color나 세부 design이 정해지면 아래 스타일 수정 or Styled-Component로 변경
-    return <input ref={ref} type="search" style={{ fontSize: 16, fontWeight: 'bold', caretColor: 'red' }} {...props} />;
+    return (
+        <input
+            width={500}
+            ref={ref}
+            type="search"
+            style={{ fontSize: 16, fontWeight: 'bold', caretColor: 'red', border: 0 }}
+            {...props}
+        />
+    );
 });
 
 export default Input;
