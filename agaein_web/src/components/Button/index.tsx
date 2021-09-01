ffcea7ad5bbccc3b236dc51412905ca0c5a3a6a0
hjinn <hojun@hojun.local>
2021-09-01 23:19:51 +0900

    Styled component TEST

--- agaein_web/src/components/Button/index.tsx
+++ agaein_web/src/components/Button/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface ButtonProps {}
 
+const StyledButton = styled.button`
+    padding: 0.375rem 0.75rem;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border: 1px solid lightgray;
+    color: gray;
+`;
+
 const Button = (props: ButtonProps) => {
-    return <div></div>;
+    return <StyledButton>버튼</StyledButton>;
 };
 
 export default Button;
--- agaein_web/src/components/NavBar/index.tsx
+++ agaein_web/src/components/NavBar/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import Button from 'components/Button';
+import DarkModeButton from 'components/DarkModeButton';
+import styled from 'styled-components';
 
 interface NavBarProps {}
 
+const NavBarContainer = styled.div`
+    width: 100vw;
+    height: 80px;
+    color: ${(props) => props.color};
+    padding-vertical: 10px;
+    flex-direction: 'row';
+    margin: 5px;
+    display: flex;
+`;
+
 const NavBar = (props: NavBarProps) => {
-    return <div></div>;
+    return (
+        // <div style={{ width: '100vw', background: '#aaaaaa' }}>
+        //     <h1>네비게이션 바</h1>
+        // </div>
+        <NavBarContainer color={'#0038ff'}>
+            <h1>이것은 NavBar입니다.</h1>
+            <DarkModeButton />
+            <Button />
+        </NavBarContainer>
+    );
 };
 
 export default NavBar;
