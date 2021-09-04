fc0ce404c4a95290b77fc0df92bcc7598a59cfe3
hjinn <hojun@hojun.local>
2021-09-05 00:22:00 +0900

    Codegen 기능 추가

---
+++ agaein_web/src/graphql/Query/User.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const fetchUser = gql`
+    query fetchUser($input: ID) {
+        user(id: $id) {
+            user {
+                name
+                email
+                password
+                kakao_id
+                phone_number
+            }
+        }
+    }
+`;
