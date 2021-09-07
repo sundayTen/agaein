ef988e6bd846a2952bde7140838720082ece1bf4
hojunin <dlsghwns@naver.com>
2021-09-08 08:58:35 +0900

    NavBar에서 로그인 버튼 기능 추가

--- agaein_web/src/components/NavBar/index.tsx
+++ agaein_web/src/components/NavBar/index.tsx
@@ -1,6 +1,10 @@
+// @ts-nocheck
+
 import React, { useState } from 'react';
 import { MenuIcon, MoonIcon, SunIcon } from '@heroicons/react/solid';
 import { DarkMode, Manu, ManuBox, Nav, Title } from './style';
+import KakaoLogin from 'react-kakao-login';
+import { KAKAO_LOGIN_KEY } from 'config/server';
 
 interface NavBarProps {}
 
@@ -18,7 +22,29 @@ const NavBar = (props: NavBarProps) => {
             <ManuBox>
                 <Title>AGAEIN</Title>
             </ManuBox>
-            <button style={{ width: 70 }}>로그인</button>
+            <KakaoLogin
+                token={KAKAO_LOGIN_KEY}
+                buttonTitle="카카오 계정으로 로그인"
+                onSuccess={(result) => {
+                    console.log(result);
+                }}
+                onFail={(result) => {
+                    console.log(result);
+                }}
+                getProfile={true}
+                useLoginForm={true}
+                style={{
+                    cursor: 'pointer',
+                    backgroundColor: 'yellow',
+                    paddingRight: 20,
+                    paddingLeft: 20,
+                    width: 200,
+                    height: 50,
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    borderRadius: 10,
+                }}
+            />
             <DarkMode onClick={() => (darkToggle ? setdarkToggle(false) : setdarkToggle(true))}>
                 {darkToggle ? (
                     <MoonIcon style={{ width: 30, height: 30 }} />
