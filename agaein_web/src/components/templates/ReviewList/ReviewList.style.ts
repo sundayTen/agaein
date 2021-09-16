e1798e0f3456f1007238008df265da2f8decffbc
hojunin <dlsghwns@naver.com>
2021-09-17 08:04:25 +0900

    Review Item direction 수정

--- agaein_web/src/components/templates/ReviewList/ReviewList.style.ts
+++ agaein_web/src/components/templates/ReviewList/ReviewList.style.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
-export const ListContainer = styled.ul``;
+export const ListContainer = styled.ul`
+    display: flex;
+    flex-direction: row;
+`;
 export const ListItem = styled.li`
     display: inline;
 `;
