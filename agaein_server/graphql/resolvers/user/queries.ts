dbdb675686ca0880144de424c246c3a1a5cbab2f
fksk94 <fksk94@gmail.com>
2021-09-17 22:45:51 +0900

    refactor: 💡 delete log

--- agaein_server/graphql/resolvers/user/queries.ts
+++ agaein_server/graphql/resolvers/user/queries.ts
@@ -5,7 +5,6 @@ const userQueries = {
     user: async (_: any, args: any) => {
         try {
             const user = await knex('user').where('id', args.id).first();
-            console.log(user.created_at);
             return user;
         } catch {
             console.error();
