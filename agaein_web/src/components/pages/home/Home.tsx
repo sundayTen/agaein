00d75ee7219d6e925c0086b0e3f65e4f584f53f6
hojunin <dlsghwns@naver.com>
2021-09-23 08:36:33 +0900

    필요없는 코드 제거

--- agaein_web/src/components/pages/home/Home.tsx
+++ agaein_web/src/components/pages/home/Home.tsx
@@ -1,7 +1,6 @@
 //@ts-nocheck
 import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
-import styled from 'styled-components';
 import { RouteComponentProps } from 'react-router-dom';
 import ReviewList from 'components/organism/ReviewList/ReviewList';
 import PostList from 'components/organism/PostList/PostList';
@@ -12,20 +11,12 @@ const Home = ({ history }: RouteComponentProps) => {
 
     return (
         <div style={{ backgroundColor: theme.light.background }}>
-            <Container>
-                <HomeHeader />
-                <PostList title="찾습니다!" />
-                <PostList title="발견했습니다!" />
-                <ReviewList />
-            </Container>
+            <HomeHeader />
+            <PostList title="찾습니다!" />
+            <PostList title="발견했습니다!" />
+            <ReviewList />
         </div>
     );
 };
-
-const Container = styled.div`
-    width: 100%;
-    max-width: 1280px;
-    margin: 0 auto;
-`;
 
 export default Home;
