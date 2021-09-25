588c2cb64d7316d12b0dfac0b58263c51bd8237e
hjinn <hojun@hojun.local>
2021-09-25 16:36:52 +0900

    Apollo Link 적용

--- agaein_web/src/graphql/apollo.ts
+++ agaein_web/src/graphql/apollo.ts
@@ -1,5 +1,6 @@
 import Cookies from 'universal-cookie';
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, from, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import { setContext } from '@apollo/client/link/context';
 
 const cookies = new Cookies();
@@ -15,7 +16,26 @@ const authLink = setContext((_, { headers }) => {
         },
     };
 });
-export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+// TODO : Error 핸들링은 여기서 함 (1. 콘솔 말고 기능 정의, 2. 에러 타입 정립)
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) => {
+            alert(`Error Occur ${message}`);
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`);
+        });
+
+    if (networkError) {
+        alert('Network Error Occur');
+        console.log(`[Network error]: ${networkError}`);
+    }
+});
+
+export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    name: 'agaein',
+    version: '0.0.1',
+    link: from([authLink, errorLink, httpLink]),
+    cache: new InMemoryCache({
+        addTypename: true, // __typename 필드를 자동으로 붙혀주는지 여부 - default : true. false로 바꾸면
+        resultCaching: true,
+    }),
 });
