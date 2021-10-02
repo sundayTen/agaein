87fee69702ef84da1c3ad1f37039d5d47fe3afa5
Haeeeun <hegfs6327@naver.com>
2021-10-02 20:06:29 +0900

    버튼 스타일 분기 정리

--- agaein_web/src/components/molecules/Button/Button.style.ts
+++ agaein_web/src/components/molecules/Button/Button.style.ts
@@ -1,25 +1,70 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface StyledButtonProps {
     buttonType: 'NORMAL' | 'BIG' | 'SMALL';
     status: 'PAINTED' | 'BORDER' | 'DISABLED';
 }
 
+const sizeStyles = css`
+    ${(props: StyledButtonProps) =>
+        props.buttonType === 'BIG' &&
+        css`
+            width: 149px;
+            height: 64px;
+            font-size: 20px;
+        `}
+
+    ${(props) =>
+        props.buttonType === 'NORMAL' &&
+        css`
+            width: 102px;
+            height: 40px;
+            font-size: 16px;
+        `}
+
+    ${(props) =>
+        props.buttonType === 'SMALL' &&
+        css`
+            width: 82px;
+            height: 32px;
+            font-size: 14px;
+        `}
+`;
+
+const colorStyles = css`
+    ${(props: StyledButtonProps) =>
+        props.status === 'PAINTED' &&
+        css`
+            background-color: props.theme.light.primary;
+            color: 'white';
+        `}
+
+    ${(props) =>
+        props.status === 'BORDER' &&
+        css`
+            border: '1px solid' + props.theme.light.primary
+            background-color: 'white';
+            color: props.theme.light.primary;
+        `}
+
+    ${(props) =>
+        props.status === 'DISABLED' &&
+        css`
+            background-color: ? props.theme.light.disable;
+            color: 'white';
+            cursor: default;
+        `}
+`;
+
 export const StyledButton = styled.button<StyledButtonProps>`
     padding: 5px 10px;
-    border-radius: 29px;
+    border-radius: 4px;
     font-size: 20px;
     line-height: 24px;
     font-weight: 700;
     user-select: none;
-    width: ${(props) => (props.buttonType === 'BIG' ? '400px' : props.buttonType === 'SMALL' ? '140px' : '200px')};
-    height: ${(props) => (props.buttonType === 'BIG' ? '180px' : props.buttonType === 'SMALL' ? '40px' : '52px')};
-    border: ${(props) => (props.status === 'BORDER' ? '1px solid' + props.theme.light.primary : undefined)};
-    background-color: ${(props) =>
-        props.status === 'PAINTED'
-            ? props.theme.light.primary
-            : props.status === 'DISABLED'
-            ? props.theme.light.disable
-            : 'white'};
-    color: ${(props) => (props.status === 'BORDER' ? props.theme.light.primary : 'white')};
+
+    ${sizeStyles}
+
+    ${colorStyles}
 `;
