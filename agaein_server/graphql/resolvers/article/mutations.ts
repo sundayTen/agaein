c3a59c62633e5c6499173507f7ea8647c4e02d06
fksk94 <fksk94@gmail.com>
2021-10-04 17:29:25 +0900

    feat: 🎸 article CR

--- agaein_server/graphql/resolvers/article/mutations.ts
+++ agaein_server/graphql/resolvers/article/mutations.ts
@@ -3,45 +3,129 @@ import { knex } from '../../database';
 
 const articleMutations = {
     createArticle: async (_: any, args: any, context: any) => {
+        // @TODO 위치 맨 뒤로 바꾸기 현재는 하나의 이미지만 가능
+        const { createReadStream, filename } = await args.file;
+
+        const stream = createReadStream();
+
+        const out = require('fs').createWriteStream(filename);
+        await stream.pipe(out);
+        await stream.on('close', () => {
+            console.log(`store ${filename}`);
+        });
+
         const now = new Date();
-        const info = {
+        const articleForm = {
             // 임시로 18 넣음
             user_id: 18,
-            info: JSON.stringify(args.Article),
+            title: args.title,
+            content: args.content,
+            created_at: now,
+            updated_at: now,
+        };
+
+        const article: any = {
+            // 유저 데이터 넣어야 함.
+            user: {},
+            title: args.title,
+            content: args.content,
+            comments: [],
             created_at: now,
             updated_at: now,
         };
 
         try {
-            if (args.boardType === 'LFP') {
-                const articles = await knex('looking_for_pet_article').insert(info).returning('*');
-                const article = articles[0];
-                return article;
-            }
-            if (args.boardType === 'LFG') {
-                const articles = await knex('looking_for_guardian_article').insert(info).returning('*');
-                const article = articles[0];
-                return article;
-            }
+            const articles = await knex('article').insert(articleForm).returning('*');
+            article.id = articles[0].id;
         } catch {
+            console.error('createArticle - article에서 에러발생');
+            console.trace();
+
+            throw new ApolloError('DataBase Server Error', 'INTERNAL_SERVER_ERROR');
+        }
+
+        const articleDetailForm: any = {
+            LFG: {
+                article_id: article.id,
+                breed_id: args.breedId,
+                name: args.name,
+                feature: args.feature,
+                gender: args.gender,
+                location: args.location,
+                foundDate: args.foundDate,
+            },
+            LFP: {
+                article_id: article.id,
+                breed_id: args.breedId,
+                name: args.name,
+                feature: args.feature,
+                gender: args.gender,
+                location: args.location,
+                lostDate: args.lostDate,
+                gratuity: args.gratuity,
+            },
+            REVIEW: {},
+        };
+
+        // @TODO validation 확인해야 됨.
+
+        try {
+            const articleDetail = await knex(args.boardType).insert(articleDetailForm[args.boardType]).returning('*');
+            articleDetailForm[args.boardType].id = articleDetail[0].id;
+        } catch {
+            console.error('createArticle - Detail에서 에러발생');
+            console.trace();
+
+            throw new ApolloError('DataBase Server Error', 'INTERNAL_SERVER_ERROR');
+        }
+
+        article.articleDetail = articleDetailForm[args.boardType];
+
+        return article;
+    },
+    createComment: async (_: any, args: any) => {
+        const now = new Date();
+        const commentForm = {
+            // 임시로 18 넣음
+            user_id: 18,
+            article_id: args.articleId,
+            content: args.content,
+            created_at: now,
+            updated_at: now,
+        };
+
+        try {
+            const comments = await knex('comment').insert(commentForm).returning('*');
+            const comment = comments[0];
+            return comment;
+        } catch {
+            console.error('createComment에서 에러발생');
+            console.trace();
+
             throw new ApolloError('DataBase Server Error', 'INTERNAL_SERVER_ERROR');
         }
     },
-    singleUpload: async (_: any, args: any) => {
-        console.log(args);
-        const { createReadStream, filename, mimetype, encoding } = await args.file;
-        console.log(filename);
+    createNestedComment: async (_: any, args: any) => {
+        const now = new Date();
+        const commentForm = {
+            // 임시로 18 넣음
+            user_id: 18,
+            comment_id: args.commentId,
+            content: args.content,
+            created_at: now,
+            updated_at: now,
+        };
 
-        const stream = createReadStream();
+        try {
+            const comments = await knex('nested_comment').insert(commentForm).returning('*');
+            const comment = comments[0];
+            return comment;
+        } catch {
+            console.error('createComment에서 에러발생');
+            console.trace();
 
-        // This is purely for demonstration purposes and will overwrite the
-        // local-file-output.txt in the current working directory on EACH upload.
-        const out = require('fs').createWriteStream(filename);
-        await stream.pipe(out);
-        await stream.on("close", () => {
-            console.log("done")
-        });
-        return { filename, mimetype, encoding };
+            throw new ApolloError('DataBase Server Error', 'INTERNAL_SERVER_ERROR');
+        }
     },
 };
 
