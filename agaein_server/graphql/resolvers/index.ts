60b990724312e39a5337bdb386e95aa6b3f1ecfa
hojunin <dlsghwns@naver.com>
2021-11-05 11:55:55 +0900

    article 쿼리 구조 개선
    Comment의 리턴타입에 author 추가

--- agaein_server/graphql/resolvers/index.ts
+++ agaein_server/graphql/resolvers/index.ts
@@ -1,27 +1,52 @@
 import { GraphQLUpload } from 'graphql-upload';
 import { userQueries, userMutations } from './user';
 import { breedQueries, breedMutations } from './breed';
-import { bookmarkQueries, bookmarkMutations } from './bookmark';
 import { articleQueries, articleMutations } from './article';
+import { knex } from '../database';
 
 const resolvers = {
-    Upload: GraphQLUpload,
-    ArticleDetail: {
-        __resolveType(obj: any) {
-            return obj.articleType;
-        },
-    },
     Query: {
         ...userQueries,
         ...articleQueries,
         ...breedQueries,
-        ...bookmarkQueries,
     },
     Mutation: {
         ...userMutations,
         ...articleMutations,
         ...breedMutations,
-        ...bookmarkMutations,
+    },
+    Upload: GraphQLUpload,
+    ArticleDetail: {
+        __resolveType(parent: any) {
+            return parent.articleType;
+        },
+    },
+    Comment: {
+        async author(parent: any) {
+            return await knex('user').where('id', `${parent.userId}`).first();
+        },
+    },
+    Article: {
+        async author(parent: any) {
+            return await knex('user').where('id', `${parent.userId}`).first();
+        },
+        async comments(parent: any) {
+            return await knex('comment').where('articleId', `${parent.id}`);
+        },
+        async images(parent: any) {
+            const articleType = parent.articleDetail.articleType;
+            const rawImages = await knex(articleType).join('image', `${articleType}.article_id`, 'image.article_id');
+            const images: any = {};
+            rawImages.forEach((rawImage: any) => {
+                if (images[rawImage.articleId]) {
+                    images[rawImage.articleId].push(rawImage.url);
+                } else {
+                    images[rawImage.articleId] = [rawImage.url];
+                }
+            });
+
+            return images[parent.id] || [];
+        },
     },
 };
 
