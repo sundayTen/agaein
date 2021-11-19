e279637b10666c690722a07c80e05c706216dad7
Haeeeun <hegfs6327@naver.com>
2021-11-19 17:00:49 +0900

    페이지 이동시 스크롤이 맨 위로 가도록

--- agaein_web/src/index.js
+++ agaein_web/src/index.js
@@ -9,6 +9,7 @@ import { client } from 'graphql/apollo';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'theme/theme';
 import { UserProvider } from 'contexts/userContext';
+import ScrollToTop from 'components/molecules/ScrollToTop/scrollToTop';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.render(
             <ApolloProvider client={client}>
                 <ThemeProvider theme={theme}>
                     <UserProvider>
+                        <ScrollToTop />
                         <App />
                     </UserProvider>
                 </ThemeProvider>
