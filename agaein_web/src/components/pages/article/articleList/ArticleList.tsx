491f1ae2186ede35ee546eb205368e8a183ac48b
hojunin <dlsghwns@naver.com>
2022-03-20 00:45:55 +0900

    북마크 기능 수정
    404처리
    
    Error 컴포넌트 (미완) + 게시글 리스트 버그 수정
    
    북마크 기능 수정
    
    404처리
    
    Error 컴포넌트 (미완) + 게시글 리스트 버그 수정
    
    콘솔제 거
    
    게시글 리스트에서 검색 기능 추가

--- agaein_web/src/components/pages/article/articleList/ArticleList.tsx
+++ agaein_web/src/components/pages/article/articleList/ArticleList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Font from 'components/molecules/Font';
 import {
     ArticleListContainer,
@@ -15,14 +15,19 @@ import { Board_Type } from 'graphql/generated/generated';
 import { Button, Input, Pagination } from 'components/molecules';
 import { getTitle } from 'utils/converter';
 import ContentsList from './ContentsList';
+import { InputRefProps } from 'components/molecules/Input/Input';
 
 // TODO : 페이지네이션 클릭 시마다 2회씩 렌더링되는 이슈, lazy query말고 pagination으로 해결할 방법을 생각해야 함
 const ArticleList = ({ history, match }: RouteComponentProps<ArticleListParams>) => {
     const { type } = match.params;
-    const [searchInput, setSearchInput] = useState<string | undefined>(undefined);
+    const inputRef = useRef<InputRefProps>(null);
+    const [searchValue, setSearchValue] = useState<string | undefined>(undefined);
     const [page, setPage] = useState(1);
     const goCreateArticlePage = () => {
         type === Board_Type.Review ? history.push('/createReview') : history.push(`/createArticle/step2/${type}`);
+    };
+    const setKeyword = () => {
+        setSearchValue(inputRef.current?.getValue());
     };
 
     return (
@@ -31,17 +36,21 @@ const ArticleList = ({ history, match }: RouteComponentProps<ArticleListParams>)
                 <Font label={getTitle(type)} fontType="h2" fontWeight="bold" />
                 <SearchBarContainer>
                     <Input
-                        value={searchInput ?? ''}
-                        onChange={(e) => setSearchInput(e.target.value)}
+                        ref={inputRef}
                         style={{ minWidth: 840, minHeight: 46 }}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                setKeyword();
+                            }
+                        }}
                     />
-                    <SearchButton>
+                    <SearchButton onClick={setKeyword}>
                         <SearchText>Search</SearchText>
                         <StyledSearchIcon />
                     </SearchButton>
                 </SearchBarContainer>
             </ArticleListHeaderContainer>
-            <ContentsList type={type} page={page} searchText={searchInput} />
+            <ContentsList type={type} page={page} searchText={searchValue} />
             <ArticleListFooter>
                 <Button label="게시글 작성" buttonStyle="PAINTED" onClick={goCreateArticlePage} />
                 <Pagination active={page} setActive={setPage} />
