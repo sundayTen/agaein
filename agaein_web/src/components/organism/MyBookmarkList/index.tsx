8463131813d956ff65aa384b66bc9b024d574f25
Nick <hojun@inhojun-ui-MacBookPro.local>
2022-03-27 00:49:19 +0900

    표현 수정

--- agaein_web/src/components/organism/MyBookmarkList/index.tsx
+++ agaein_web/src/components/organism/MyBookmarkList/index.tsx
@@ -1,7 +1,11 @@
 import { BookmarkList, BookmarkItem } from 'components/pages/myPage/MyPage.style';
 import PostItem from 'components/molecules/PostItemBox/PostItemBox';
+import { useContext } from 'react';
+import { BookmarkContext } from 'contexts';
 
 const MyBookmarkList = () => {
+    const { isBookmarked, setBookmark } = useContext(BookmarkContext);
+
     return (
         <BookmarkList>
             <BookmarkItem>
