1b501931e0d950cbaf72baf8970a0cbdb65a49fd
hojunin <dlsghwns@naver.com>
2022-10-10 21:28:19 +0900

    feat: GA 설정

--- agaein_web/src/App.tsx
+++ agaein_web/src/App.tsx
@@ -1,8 +1,10 @@
 import GlobalStyle from './assets/styles/GlobalStyle';
 import AppLayout from 'components/organism/AppLayout/AppLayout';
 import Router from 'router';
+import useGATracking from 'useGATraking';
 
 function App() {
+    useGATracking();
     return (
         <>
             <GlobalStyle />
