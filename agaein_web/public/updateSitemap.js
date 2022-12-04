c21d391386119580503d54f836e0d1f7dd2766aa
hojunin <dlsghwns@naver.com>
2022-12-04 15:44:39 +0900

    feat: 사이트맵 제너레이터 변경

--- agaein_web/public/updateSitemap.js
+++ agaein_web/public/updateSitemap.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 const today = new Date();
-const todayWithFormatDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+const date = today.getDate().toLocaleString();
+const convertedDate = date.length === 1 ? '0' + date : date;
+const todayWithFormatDate = `${today.getFullYear()}-${today.getMonth() + 1}-${convertedDate}`;
 
 const updateLastMod = (path) =>
     fs.readFileSync(path, { encoding: 'utf-8' }).replace(/\d{4}-\d{2}-\d{0,2}/g, todayWithFormatDate);
