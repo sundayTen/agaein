8896cd98978b535a3c41cf1d80c2033bf934d156
hojunin <dlsghwns@naver.com>
2023-01-20 09:31:47 +0900

    feat: 사이트맵 생성로직 변경 및 업데이트

--- agaein_web/public/updateSitemap.js
+++ agaein_web/public/updateSitemap.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
+const enhanceDateFormat = (d) => {
+    const dateToString = String(d);
+    if (dateToString.length === 1) {
+        return '0' + dateToString;
+    }
+    return dateToString;
+};
+
+// 크론탭 실행 날짜 및 포매팅된 날짜.
 const today = new Date();
-const date = today.getDate().toLocaleString();
-const convertedDate = date.length === 1 ? '0' + date : date;
-const todayWithFormatDate = `${today.getFullYear()}-${today.getMonth() + 1}-${convertedDate}`;
+const todayWithFormatDate = `${today.getFullYear()}-${enhanceDateFormat(today.getMonth() + 1)}-${enhanceDateFormat(
+    today.getDate().toLocaleString(),
+)}`;
 
 const updateLastMod = (path) =>
     fs.readFileSync(path, { encoding: 'utf-8' }).replace(/\d{4}-\d{2}-\d{0,2}/g, todayWithFormatDate);
